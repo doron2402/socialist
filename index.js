598d31e7bbd29666339ed707e3dce9cbbad0ad25
Doron Segal <doron2402@gmail.com>
2014-07-30 12:21:33 -0700

    updte

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ if (process.argv['env']) {
 
 
 //Modules
-AA.Hapi = require("hapi");
+var Hapi = require("hapi");
 AA.Joi = require("joi");
 AA.Mongo = require('mongoose');
 AA.async = require('async');
@@ -26,7 +26,9 @@ AA._ = require('lodash');
 //Settings
 AA.Settings = require('./settings')({env: environment});
 
+AA.MongoDB = AA.Mongo.connect(AA.Settings.MONGODB.URL);
 AA.Routes = require('./routes');
+AA.Schema = require('./schema');
 AA.Models = require('./models');
 
 AA.server = new Hapi.Server(AA.Settings.PORT, AA.Settings.HOST, AA.Settings.OPTIONS);
